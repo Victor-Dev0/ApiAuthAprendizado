b7c71a14026f2a552ebdaed1205555dac75a095f
Victor-Dev0 <vitinhomatascav@hotmail.com>
2025-06-13 21:02:30 -0300

    Primeiro Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using ApiAuth.Models;
+using ApiAuth.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public LoginController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("logar")]
+        public async Task<ActionResult> Login(Usuario usuario)
+        {
+            try
+            {
+                var token = _tokenService.GerarToken(usuario);
+
+                return Ok(new { UsuarioLogado = usuario, Token = token });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,46 @@
+﻿using ApiAuth.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiAuth.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GerarToken(Usuario usuario)
+        {
+            var chave = Encoding.UTF8.GetBytes(_configuration["JwtConfig:Key"]!);
+
+            var handler = new JwtSecurityTokenHandler();
+                        
+            var credencial = new SigningCredentials(new SymmetricSecurityKey(chave), SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = GerarClaims(usuario),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = credencial,
+            };
+
+            var token = handler.CreateToken(tokenDescriptor);
+            var strToken = handler.WriteToken(token);
+
+            return strToken;
+        }
+
+        private static ClaimsIdentity GerarClaims(Usuario usuario)
+        {
+            var ci = new ClaimsIdentity();
+            ci.AddClaim(new Claim(ClaimTypes.Name, usuario.Email!));
+
+            return ci;
+        }
+    }
+}
